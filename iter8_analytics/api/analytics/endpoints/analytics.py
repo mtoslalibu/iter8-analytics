42e530a60af0d205b02387b776a476a84bb7c35d
Sushma Ravichandran <sushma.ravichandran@ibm.com>
2019-04-30 14:46:07 -0400

    and more

--- iter8_analytics/api/analytics/endpoints/analytics.py
+++ iter8_analytics/api/analytics/endpoints/analytics.py
@@ -64,7 +64,7 @@ class CanaryCheckAndIncrement(Resource):
         print(self.response["assessment"]["success_criteria"])
 
     def create_response_object(self, payload):
-        """Create response object corresponding to payload. This has everything."""
+        """Create response object corresponding to payload. This has everything and more."""
         self.response = {
             "metric_backend_url": prom_config["metric_backend_url"],
             "canary": {
