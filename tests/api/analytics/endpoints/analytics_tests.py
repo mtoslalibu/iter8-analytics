660ba0e7b800a331d21a7df4a3c4c83778f85012
Sushma Ravichandran <sushma.ravichandran@ibm.com>
2019-05-01 16:25:18 -0400

    Added test case

--- tests/api/analytics/endpoints/analytics_tests.py
+++ tests/api/analytics/endpoints/analytics_tests.py
@@ -5,6 +5,7 @@ import json
 from iter8_analytics import app as flask_app
 from iter8_analytics.api.analytics import responses as responses
 from iter8_analytics.api.analytics import request_parameters as request_parameters
+import dateutil.parser as parser
 
 import logging
 log = logging.getLogger(__name__)
@@ -105,3 +106,42 @@ class TestAnalyticsAPI(unittest.TestCase):
 
         assert resp.is_json
         self.assertEquals(json.loads(resp.get_data()), expected_response)
+
+        def test_start_and_end_time(self):
+            """Tests the REST endpoint /analytics/canary/check_and_increment."""
+
+            endpoint = '/api/v1/analytics/canary/check_and_increment'
+            log.info('===TESTING ENDPOINT {endpoint}'.format(endpoint=endpoint))
+
+            #####
+            # Test request with start time less than end time
+
+            parameters = {
+                "baseline": {
+                    "start_time": "2019-04-24T19:40:32.017Z",
+                    "end_time": "2019-04-24T19:40:32.017Z",
+                    "tags": {
+                        "destination_service_name": "reviews-v2"
+                    }
+                },
+                "canary": {
+                    "start_time": "2019-04-24T19:40:32.017Z",
+                    "end_time": "2019-04-24T19:40:32.017Z",
+                    "tags": {
+                        "destination_service_name": "reviews-v2"
+                    }
+                },
+                "traffic_control": {
+                    "success_criteria": [
+                        {
+                            "metric_name": "iter8_latency",
+                            "type": "delta",
+                            "value": 0.02
+                        }
+                    ]
+                },
+                "_last_state": {}
+            }
+            resp = self.flask_test.post(endpoint, json=parameters)
+            self.assertEqual(resp.status_code, 400,
+                             f'Expected a 400 HTTP code, but received a {resp.status_code}')
