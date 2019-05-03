80b99e8bf896bb1aab7f8aaa731605e916d62bcd
Sushma Ravichandran <sushma.ravichandran@ibm.com>
2019-05-03 10:38:46 -0400

    fixed bug

--- iter8_analytics/metrics_backend/iter8metric.py
+++ iter8_analytics/metrics_backend/iter8metric.py
@@ -41,11 +41,11 @@ class Iter8MetricFactory:
         now = datetime.now(timezone.utc)
         if start < end:
             interval = end-start
-            interval_str = str(interval.total_seconds()) + "s"
+            interval_str = str(int(interval.total_seconds())) + "s"
             if end < now:
                 offset = now-end
                 if offset.total_seconds() > 0:
-                    offset_str = str(offset.total_seconds()) + "s"
+                    offset_str = str(int(offset.total_seconds())) + "s"
             else:
                 offset_str = ""
         else:
