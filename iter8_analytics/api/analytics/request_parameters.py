046848e1574edb82d3c0e27f803d7b76f60eeec3
Sushma Ravichandran <sushma.ravichandran@ibm.com>
2020-03-03 13:05:42 -0500

    added last state to pbr/obr + refactored experiment object

--- iter8_analytics/api/analytics/request_parameters.py
+++ iter8_analytics/api/analytics/request_parameters.py
@@ -290,6 +290,6 @@ bayesian_routing_parameters = api.model('bayesian_routing_parameters', {
          traffic_control_br, required=True,
          description='Parameters controlling the behavior of the analytics'),
      LAST_STATE_STR: fields.Raw(
-         required=False,
+         required=True,
          description='State returned by the server on the previous call')
  })
