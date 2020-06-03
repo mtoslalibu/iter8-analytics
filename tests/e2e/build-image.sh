d1577344e4e102e6dad7667f5707c31432321dcd
Hai Huang <huang195@users.noreply.github.com>
2020-06-03 14:32:31 -0400

    use simpler image tag (#95)

--- tests/e2e/build-image.sh
+++ tests/e2e/build-image.sh
@@ -2,18 +2,18 @@
 
 # Only build images for master and v[0-9]+.[0-9]+ release branches
 if [[ "$TRAVIS_BRANCH" == "master" ]] || [[ "$TRAVIS_BRANCH" =~ ^v[0-9]+\.[0-9]+$ ]] ; then
-  echo $DOCKERHUB_TOKEN | docker login -u $DOCKERHUB_USERNAME --password-stdin;
   # Only build images when commits are done on these branches
   if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-    export IMG="iter8/iter8-analytics:$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT";
-    echo "Building Docker image - $IMG";
+    echo $DOCKERHUB_TOKEN | docker login -u $DOCKERHUB_USERNAME --password-stdin;
+    export IMG="iter8/iter8-analytics:$TRAVIS_BRANCH";
+    echo "Building PR Docker image - $IMG";
     make docker-build;
     make docker-push;
-    LATEST="iter8/iter8-analytics:latest";
-    echo "Tagging image as latest - $LATEST";
-    docker tag $IMG $LATEST;
-    export IMG=$LATEST;
-    make docker-push;
+    #LATEST="iter8/iter8-analytics:latest";
+    #echo "Tagging image as latest - $LATEST";
+    #docker tag $IMG $LATEST;
+    #export IMG=$LATEST;
+    #make docker-push;
   fi
 fi
 
