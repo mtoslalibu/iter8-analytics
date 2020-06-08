4f96c6d358e1d4c9420c0438ee8fe1259be89089
Hai Huang <huang195@users.noreply.github.com>
2020-06-08 16:20:48 -0400

    optimize travis (#103)

--- tests/e2e/build-image.sh
+++ tests/e2e/build-image.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
-# Only build images for master and v[0-9]+.[0-9]+ release branches
-if [[ "$TRAVIS_BRANCH" == "master" ]] || [[ "$TRAVIS_BRANCH" =~ ^v[0-9]+\.[0-9]+$ ]] ; then
-  # Only build images when commits are done on these branches
-  if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-    echo $DOCKERHUB_TOKEN | docker login -u $DOCKERHUB_USERNAME --password-stdin;
-    export IMG="iter8/iter8-analytics:$TRAVIS_BRANCH";
-    echo "Building Docker image - $IMG";
-    make docker-build;
-    make docker-push;
-    LATEST="iter8/iter8-analytics:latest";
-    echo "Tagging image as latest - $LATEST";
-    docker tag $IMG $LATEST;
-    export IMG=$LATEST;
-    make docker-push;
-  fi
-fi
+# Build the image only when this is triggered by a "Branch build", i.e., PR
+# merge. This is checked in .travis.yml
+echo $DOCKERHUB_TOKEN | docker login -u $DOCKERHUB_USERNAME --password-stdin;
+export IMG="iter8/iter8-analytics:$TRAVIS_BRANCH";
+echo "Building Docker image - $IMG";
+make docker-build;
+make docker-push;
+LATEST="iter8/iter8-analytics:latest";
+echo "Tagging image as latest - $LATEST";
+docker tag $IMG $LATEST;
+export IMG=$LATEST;
+make docker-push;
